694fff8939661826809d767fbb1df7c5b5224f1f
Jason Whitby <john.bays-wit@protonmail.com>
2022-07-27 00:34:54 -0500

    Initial commit to master branch.

--- src/main/java/com/example/dogdbtest/mysqlconnect.java
+++ src/main/java/com/example/dogdbtest/mysqlconnect.java
@@ -8,24 +8,24 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class mysqlconnect {
-    private static final String protocol = "jdbc";
-    private static final String vendorName = ":mysql:";
-    private static final String ipAddress = "//localhost:3306";
-    private static final String dbName = "usersdb";
-
-    private static final String jdbcURL = protocol + vendorName + ipAddress + "/" + dbName;
-
-    private static final String MYSQLJBCDriver = "com.mysql.jdbc.Driver";
-
-    private static final String username = "root";
-    private static final String password = "toor";
-    private static Connection conn = null;
+//    private static final String protocol = "jdbc";
+//    private static final String vendorName = ":mysql:";
+//    private static final String ipAddress = "//localhost:3306";
+//    private static final String dbName = "usersdb";
+//
+//    private static final String jdbcURL = protocol + vendorName + ipAddress + "/" + dbName;
+//
+//    private static final String MYSQLJBCDriver = "com.mysql.jdbc.Driver";
+//
+//    private static final String username = "root";
+//    private static final String password = "toor";
+//    private static Connection conn = null;
+    Connection conn = null;
     public static Connection ConnectDb() throws ClassNotFoundException {
 
         try{
-            Class.forName(MYSQLJBCDriver);
-            conn = DriverManager.getConnection(jdbcURL, username, password);
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/usersdb");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/usersdb","root","toor");
 
             System.out.println("Connection Established");
             return conn;
